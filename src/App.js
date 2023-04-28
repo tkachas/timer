61229f8e56dfb38655d6d7ce5a28203a1fd4714f
Artem Tkachenko <111357634+tkachas@users.noreply.github.com>
2023-04-28 15:43:59 +0300

    timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
 
 function App() {
+  const [time, setTime] = useState("00:00:00");
+  const [timer, setTimer] = useState(null);
+  const [elapsedSeconds, setElapsedSeconds] = useState(0);
+
+  const formatTime = useCallback((seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return (
+      String(hours).padStart(2, "0") +
+      ":" +
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(remainingSeconds).padStart(2, "0")
+    );
+  }, []);
+
+  const startTimer = () => {
+    if (timer) return;
+
+    const newTimer = setInterval(() => {
+      setElapsedSeconds((prevElapsedSeconds) => {
+        const newElapsedSeconds = prevElapsedSeconds + 1;
+        setTime(formatTime(newElapsedSeconds));
+        return newElapsedSeconds;
+      });
+    }, 1000);
+
+    setTimer(newTimer);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(timer);
+    setTimer(null);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timer);
+    setTimer(null);
+    setTime("00:00:00");
+    setElapsedSeconds(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timer]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Таймер</h1>
+      <h2>{time}</h2>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={pauseTimer}>Pause</button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
